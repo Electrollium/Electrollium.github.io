ba49c2ccc3a08a3a09d026f79fe875bd80682dba
Electrollium <69403267+Electrollium@users.noreply.github.com>
2025-05-08 16:56:47 -0500

    updated compile script

--- _src/static_compile.py
+++ _src/static_compile.py
@@ -1,11 +1,16 @@
 from includes.page_index import *
+from includes.page_projects import *
+from includes.page_contact import *
+from includes.page_resume import *
+
 
 page_config = [
-    ['index.html',gen_page_index()]
+    ['index.html',gen_page_index()],
+    ['projects.html',gen_page_projects()],
+    ['contact.html',gen_page_contact()],
+    ['resume.html',gen_page_resume()]
     ]
 
-
 for page in page_config:
-    
-    with open(page[0],'w') as file:
-        file.write(page[1])
+    with open('../'+page[0],'w') as file: #page[0] is the file name
+        file.write(page[1])#generation output
